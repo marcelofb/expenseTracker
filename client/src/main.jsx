bf0345dd52a77954bca726397895fb6ccba6ff3e
Marcelo Burriel <marceloburriel@gmail.com>
2026-08-07 15:51:15 -0300

    Add update app immediate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App.jsx';
 import './index.css';
+
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    updateSW(true);
+  }
+});
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
